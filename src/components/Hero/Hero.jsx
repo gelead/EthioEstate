a1776499ddcd03c6a15ca437d9eedef58785cb43
Gelead <Geleadw@gmail.com>
2025-08-03 12:14:03 +0300

    added residencies home card sliders

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -28,21 +28,21 @@ const Hero = () => {
                 <CountUp start={8800} end={9000} duration={4} />
                 <span>+</span>
               </span>
-              <span className="secondaryText" >Premium Products</span>
+              <span className="secondaryText stat-des" >Premium Products</span>
             </div>
             <div className="flexColCenter stat">
               <span>
                 <CountUp start={1950} end={2000} duration={4} />
                 <span>+</span>
               </span>
-              <span className="secondaryText">Happy customers</span>
+              <span className="secondaryText stat-des">Happy customers</span>
             </div>
             <div className="flexColCenter stat">
               <span>
                 <CountUp end={28} duration={4} />
                 <span>+</span>
               </span>
-              <span className="secondaryText">Award Winning</span>
+              <span className="secondaryText stat-des">Award Winning</span>
             </div>
           </div>
         </div>
